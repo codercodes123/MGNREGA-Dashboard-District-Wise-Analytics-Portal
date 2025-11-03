3c3660b3dc078933e46c8f35f4ca41b1a3b16193
codercodes123 <apjain2004@gmail.com>
2025-11-03 19:38:13 +0530

    updated new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { AnimatePresence } from 'framer-motion';
+import { Analytics } from '@vercel/analytics/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
@@ -47,6 +48,7 @@ function App() {
       
       {/* Comprehensive Footer */}
       <Footer />
+      <Analytics />
     </div>
   );
 }
